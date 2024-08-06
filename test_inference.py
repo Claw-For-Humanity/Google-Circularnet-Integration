f6de3a2d1f99fc2e6bf17fe3bdd34f810ef41d6e
Eric Kang <erickdhdock@gmail.com>
2024-08-06 15:24:54 -0700

    Update test_inference.py

--- test_inference.py
+++ test_inference.py
@@ -1,3 +1,14 @@
+import os
 import time
+import serverCom
+import cv2
 
-#TODO: time start -> inference -> time end -> calc time.
+
+class initialize:
+    def init():
+        # initialize servercom plugin        
+        serverCom.initializer.__init__(os.getcwd(),'gen-lang-client-0521940196-93d8c9849e87','1mTU6uOBwlx7VCEKP-Y038Ucm8aLXnpeToN1ddxqGCe4')
+
+
+
+time.time()
